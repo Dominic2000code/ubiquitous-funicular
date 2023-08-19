c0c965c26f0a834c3ffd0f6578d00cc55a98fb33
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2023-08-19 18:29:15 +0000

    added urls for auth and users

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
+from django.urls import include, path, re_path
+from dj_rest_auth.views import PasswordResetConfirmView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/auth/', include('dj_rest_auth.urls')),
+    path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('api/auth/password/reset/confirm/<str:uidb64>/<str:token>', PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('api/', include('api.urls')),
+    path('documentation/', include_docs_urls(title="Social App API")),
+    path('schema/', get_schema_view(
+        title="Social App API",
+        description="API documentation for social app",
+        version="1.0.0",
+        public=True,
+        url='http://127.0.0.1:8000/'
+    ), name="openapi-schema"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)

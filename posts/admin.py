2d248a64fac57023bc8dbdaa94eb8213a95a3f6f
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2023-08-20 15:29:01 +0000

    added posts models to admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from polymorphic.admin import PolymorphicParentModelAdmin, PolymorphicChildModelAdmin
 
-# Register your models here.
+from .models import Post, TextPost, ImagePost, VideoPost
+
+
+class PostChildAdmin(PolymorphicChildModelAdmin):
+    base_model = Post  # The parent model
+
+
+@admin.register(Post)
+class PostAdmin(PolymorphicParentModelAdmin):
+    base_model = Post  # The parent model
+    child_models = (TextPost, ImagePost, VideoPost)
+
+
+@admin.register(TextPost)
+class TextPostAdmin(PostChildAdmin):
+    base_model = TextPost
+    show_in_index = True
+
+
+@admin.register(ImagePost)
+class ImagePostAdmin(PostChildAdmin):
+    base_model = ImagePost
+    show_in_index = True
+
+
+@admin.register(VideoPost)
+class VideoPostAdmin(PostChildAdmin):
+    base_model = VideoPost
+    show_in_index = True

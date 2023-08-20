03d06fea9145cd8f0995808b4ca5fafed601184b
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2023-08-20 15:29:56 +0000

    added posts models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from polymorphic.models import PolymorphicModel
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Post(PolymorphicModel):
+    User = get_user_model()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author}'s post {self.id}"
+
+
+class TextPost(Post):
+    content = models.TextField()
+
+    def __str__(self):
+        return f"{self.author}'s text post"
+
+
+class ImagePost(Post):
+    image = models.ImageField(upload_to='post_images/')
+
+    def __str__(self):
+        return f"{self.author}'s image post"
+
+
+class VideoPost(Post):
+    video = models.FileField(upload_to='post_videos/')
+
+    def __str__(self):
+        return f"{self.author}'s video post"

61da42ddebf34f52c2bff150c66188c3d6eeeec3
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2023-08-20 21:10:16 +0000

    added url to toggle likes

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,8 @@ from .views import (CustomUserDetailView, CustomUserListCreateView,
                     TextPostCreateView, TextPostDetailView,
                     ImagePostCreateView, ImagePostDetailView,
                     VideoPostCreateView, VideoPostDetailView,
-                    PostListView, PostDetailView,)
+                    PostListView, PostDetailView,
+                    ToggleLikeView)
 
 app_name = 'api'
 
@@ -15,21 +16,24 @@ urlpatterns = [
     path('users/', CustomUserListCreateView.as_view(), name='user-list-create'),
     path('users/<int:pk>/', CustomUserDetailView.as_view(), name='user-detail'),
 
-    path('text-posts/', TextPostCreateView.as_view(),
+    path('posts/text-posts/', TextPostCreateView.as_view(),
          name='textpost-list-create'),
-    path('text-posts/<int:pk>/', TextPostDetailView.as_view(),
+    path('posts/text-posts/<int:pk>/', TextPostDetailView.as_view(),
          name='textpost-detail'),
 
-    path('image-posts/', ImagePostCreateView.as_view(),
+    path('posts/image-posts/', ImagePostCreateView.as_view(),
          name='imagepost-list-create'),
-    path('image-posts/<int:pk>/', ImagePostDetailView.as_view(),
+    path('posts/image-posts/<int:pk>/', ImagePostDetailView.as_view(),
          name='imagepost-detail'),
 
-    path('video-posts/', VideoPostCreateView.as_view(),
+    path('posts/video-posts/', VideoPostCreateView.as_view(),
          name='videopost-list-create'),
-    path('video-posts/<int:pk>/', VideoPostDetailView.as_view(),
+    path('posts/video-posts/<int:pk>/', VideoPostDetailView.as_view(),
          name='videopost-detail'),
 
     path('posts/', PostListView.as_view(), name='post-list'),
     path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+
+    path('posts/toggle-like/<int:post_id>/',
+         ToggleLikeView.as_view(), name='toggle-like'),
 ]

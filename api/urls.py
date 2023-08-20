5e2714b0f774a03313cea593a1714bd74d0f570e
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2023-08-20 20:01:40 +0000

    modified urls

--- api/urls.py
+++ api/urls.py
@@ -15,15 +15,18 @@ urlpatterns = [
     path('users/', CustomUserListCreateView.as_view(), name='user-list-create'),
     path('users/<int:pk>/', CustomUserDetailView.as_view(), name='user-detail'),
 
-    path('text-posts/', TextPostCreateView.as_view(), name='textpost-create'),
+    path('text-posts/', TextPostCreateView.as_view(),
+         name='textpost-list-create'),
     path('text-posts/<int:pk>/', TextPostDetailView.as_view(),
          name='textpost-detail'),
 
-    path('image-posts/', ImagePostCreateView.as_view(), name='imagepost-create'),
+    path('image-posts/', ImagePostCreateView.as_view(),
+         name='imagepost-list-create'),
     path('image-posts/<int:pk>/', ImagePostDetailView.as_view(),
          name='imagepost-detail'),
 
-    path('video-posts/', VideoPostCreateView.as_view(), name='videopost-create'),
+    path('video-posts/', VideoPostCreateView.as_view(),
+         name='videopost-list-create'),
     path('video-posts/<int:pk>/', VideoPostDetailView.as_view(),
          name='videopost-detail'),
 

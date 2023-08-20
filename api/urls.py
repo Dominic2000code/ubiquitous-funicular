6331154dd3b6e15542d68e2d3b6fd66963ba6b16
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2023-08-20 15:29:44 +0000

    added urls for posts views

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CustomUserDetailView, CustomUserListCreateView
+from .views import (CustomUserDetailView, CustomUserListCreateView,
+                    TextPostCreateView, TextPostDetailView,
+                    ImagePostCreateView, ImagePostDetailView,
+                    VideoPostCreateView, VideoPostDetailView,
+                    PostListView, PostDetailView,)
 
 app_name = 'api'
 
@@ -10,4 +14,19 @@ router = DefaultRouter()
 urlpatterns = [
     path('users/', CustomUserListCreateView.as_view(), name='user-list-create'),
     path('users/<int:pk>/', CustomUserDetailView.as_view(), name='user-detail'),
+
+    path('text-posts/', TextPostCreateView.as_view(), name='textpost-create'),
+    path('text-posts/<int:pk>/', TextPostDetailView.as_view(),
+         name='textpost-detail'),
+
+    path('image-posts/', ImagePostCreateView.as_view(), name='imagepost-create'),
+    path('image-posts/<int:pk>/', ImagePostDetailView.as_view(),
+         name='imagepost-detail'),
+
+    path('video-posts/', VideoPostCreateView.as_view(), name='videopost-create'),
+    path('video-posts/<int:pk>/', VideoPostDetailView.as_view(),
+         name='videopost-detail'),
+
+    path('posts/', PostListView.as_view(), name='post-list'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
 ]

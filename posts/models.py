37c083577b45c5b0a217d43bb44303f6539507e9
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2023-08-20 21:11:07 +0000

    added likes field

--- posts/models.py
+++ posts/models.py
@@ -7,6 +7,7 @@ class Post(PolymorphicModel):
     User = get_user_model()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='liked_posts')
 
     def __str__(self):
         return f"{self.author}'s post {self.id}"

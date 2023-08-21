b445d9caa1b006c5c97f8e7e0a95229ca0540449
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2023-08-21 15:27:03 +0000

    added Repost model

--- posts/models.py
+++ posts/models.py
@@ -2,15 +2,26 @@ from django.db import models
 from polymorphic.models import PolymorphicModel
 from django.contrib.auth import get_user_model
 
+User = get_user_model()
+
 
 class Post(PolymorphicModel):
-    User = get_user_model()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField(User, related_name='liked_posts')
+    repost_count = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return f"{self.author}'s post {self.id}"
+
+
+class Repost(models.Model):
+    original_post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"Repost of {self.original_post.author}'s post by {self.user}"
 
 
 class TextPost(Post):

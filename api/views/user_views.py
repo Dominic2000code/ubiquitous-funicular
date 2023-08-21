682e6bf9cbc65057be960b6f801a6482602dee36
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2023-08-21 21:08:55 +0000

    modified FollowerListView

--- api/views/user_views.py
+++ api/views/user_views.py
@@ -8,6 +8,7 @@ from users.serializers import CustomUserSerializer, FollowSerializer
 from django.contrib.auth import get_user_model
 from django.conf import settings
 from django.core.cache import cache
+from django.shortcuts import get_object_or_404
 import redis
 
 r = redis.Redis(host=settings.REDIS_HOST,
@@ -55,18 +56,33 @@ class ToggleFollowView(APIView):
             return Response({'detail': f'You are now following {user.username}.'}, status=status.HTTP_201_CREATED)
 
 
-class FollowerListView(generics.ListAPIView):
-    serializer_class = CustomUserSerializer
+class FollowerListView(APIView):
     permission_classes = [IsAuthenticated]
 
-    def get_queryset(self):
-        user_id = self.kwargs['user_id']
+    def get(self, request, user_id, format=None):
+        cached_data = cache.get(f'followers_data_{user_id}')
+        if cached_data:
+            return Response(cached_data)
 
-        followers = cache.get(f'followers_{user_id}')
-
-        if followers is None:
-            followers = Follow.objects.filter(user=user_id)
-            cache.set(f'followers_{user_id}', followers)
-
+        user = get_object_or_404(CustomUser, id=user_id)
+        followers = Follow.objects.filter(user=user)
         follower_ids = followers.values_list('follower_id', flat=True)
-        return User.objects.filter(id__in=follower_ids)
+        follower_users = CustomUser.objects.filter(id__in=follower_ids)
+        follower_data = []
+        total_followers_count = Follow.get_follower_count(user.id)
+
+        for follower in follower_users:
+            data = {
+                'follower': CustomUserSerializer(follower).data,
+                'followed_at': followers.get(follower=follower).created_at,
+                'followers_count': Follow.get_follower_count(follower.id)
+            }
+            follower_data.append(data)
+
+        response_data = {
+            'followers': follower_data,
+            'followers_count': total_followers_count
+        }
+        cache.set(f'followers_data_{user_id}', response_data, 3600)
+
+        return Response(response_data)

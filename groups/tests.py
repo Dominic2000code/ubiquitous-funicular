5c9ba00e9ad1f6b981de701e8331d73ae5b2acdd
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2023-08-22 18:18:22 +0000

    added tests for group models

--- groups/tests.py
+++ groups/tests.py
@@ -1,3 +1,46 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Group, Membership, GroupPost
 
-# Create your tests here.
+User = get_user_model()
+
+
+class GroupModelTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser', password='testpass'
+        )
+        self.group = Group.objects.create(
+            name='Test Group', description='This is a test group', creator=self.user
+        )
+        Membership.objects.create(user=self.user, group=self.group)
+
+    def test_group_str(self):
+        self.assertEqual(str(self.group), f'Group owner: {self.group.name}')
+
+    def test_membership_created(self):
+        membership = Membership.objects.get(user=self.user, group=self.group)
+        self.assertIsNotNone(membership)
+
+    def test_group_has_member(self):
+        self.assertIn(self.user, self.group.members.all())
+
+
+class GroupPostModelTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser', password='testpassword')
+        self.group = Group.objects.create(
+            name='Test Group', description='This is a test group',
+            creator=self.user)
+        self.group_post = GroupPost.objects.create(
+            group=self.group, author=self.user, content='Test content')
+
+    def test_group_post_str(self):
+        self.assertEqual(
+            str(self.group_post),
+            f'Group Post by {self.user} in {self.group}'
+        )
+
+    def test_group_post_has_content(self):
+        self.assertEqual(self.group_post.content, 'Test content')

6d097409527472fe0a4edb626a913e8aec4c8448
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2023-08-26 06:39:30 +0000

    added permission for account owner edit

--- api/permissions.py
+++ api/permissions.py
@@ -14,3 +14,18 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
 
         # Write permissions are only allowed to the author of the post.
         return obj.author == request.user
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to allow only the account owner edit account.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the author of the post.
+        return obj.pk == request.user.pk

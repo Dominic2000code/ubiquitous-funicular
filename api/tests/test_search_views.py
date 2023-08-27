bdf4f715d84466f86d0c07500430bcd9c8112875
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2023-08-27 11:44:16 +0000

    mod

--- api/tests/test_search_views.py
+++ api/tests/test_search_views.py
@@ -1,6 +1,5 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase
-from rest_framework import status
 from django.contrib.auth import get_user_model
 from posts.models import TextPost
 from groups.models import Group
@@ -25,7 +24,7 @@ class SearchViewTests(APITestCase):
     def test_search_view(self):
         url = reverse('api:search')
         response = self.client.get(url, {'q': 'test'})
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, 200)
 
         expected_data = {
             'users': [CustomUserSerializer(self.user).data],
@@ -37,7 +36,7 @@ class SearchViewTests(APITestCase):
     def test_user_search_view(self):
         url = reverse('api:user-search')
         response = self.client.get(url, {'q': 'testuser'})
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, 200)
 
         expected_data = [CustomUserSerializer(self.user).data]
         self.assertEqual(response.data, expected_data)
@@ -45,7 +44,7 @@ class SearchViewTests(APITestCase):
     def test_post_search_view(self):
         url = reverse('api:post-search')
         response = self.client.get(url, {'q': 'test'})
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, 200)
 
         expected_data = [TextPostSerializer(self.text_post).data]
         self.assertEqual(response.data, expected_data)
@@ -53,7 +52,7 @@ class SearchViewTests(APITestCase):
     def test_group_search_view(self):
         url = reverse('api:group-search')
         response = self.client.get(url, {'q': 'test'})
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, 200)
 
         expected_data = [GroupSerializer(self.group).data]
         self.assertEqual(response.data, expected_data)
